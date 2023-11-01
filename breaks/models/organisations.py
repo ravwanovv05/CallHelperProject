912eb7c0b883bc65bb473c75a7c0fddad2e0320f
ravwanovv05 <samidilloravshanov14@gmail.com>
2023-11-01 17:59:36 +0500

    fix: created groups.py

--- breaks/models/organisations.py
+++ breaks/models/organisations.py
@@ -24,37 +24,3 @@ class Organisation(models.Model):
         return self.name
 
 
-class Group(models.Model):
-    organisation = models.ForeignKey(
-        'breaks.Organisation', models.CASCADE, 'groups',
-        verbose_name='Organisation'
-    )
-    name = models.CharField('Name', max_length=255)
-    manager = models.ForeignKey(
-        User, models.RESTRICT, 'group_managers',
-        verbose_name='Manager'
-    )
-    employees = models.ManyToManyField(
-        User, 'group_employees', verbose_name='Employees',
-        blank=True
-    )
-    min_active = models.PositiveSmallIntegerField(
-        'Minimum number of active employees', null=True, blank=True
-    )
-    break_start = models.TimeField('Start break', null=True, blank=True)
-    break_end = models.TimeField('Break end', null=True, blank=True)
-    break_max_duration = models.PositiveSmallIntegerField(
-        'Minimum break duration', null=True, blank=True
-    )
-
-    class Meta:
-        verbose_name = 'Group'
-        verbose_name_plural = 'Groups'
-        ordering = ('name',)
-
-    def __str__(self):
-        return self.name
-
-    @property
-    def break_duration(self):
-        return 500
